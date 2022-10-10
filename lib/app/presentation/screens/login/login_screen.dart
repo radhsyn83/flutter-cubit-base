8ed4fc0f8741b2654de2a291e65630832ebdd9ff
Fathur Radhy <radhsyn83@gmail.com>
2022-10-10 16:01:15 +0700

    refactoring

--- lib/app/presentation/screens/login/login_screen.dart
+++ lib/app/presentation/screens/login/login_screen.dart
@@ -1,10 +1,8 @@
-import 'package:cubit_core/app/cubit/main_cubit.dart';
 import 'package:cubit_core/app/cubit/default_state.dart';
 import 'package:cubit_core/app/data/models/login_model.dart';
 import 'package:cubit_core/app/presentation/screens/base_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:cubit_core/app/presentation/screens/login/login_cubit.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
